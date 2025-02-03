87c7c354589af154547be49c0006af0b4bde6ebb
krupa patel <krupa@promactinfo.com>
2025-02-03 15:57:35 +0530

    Resolves build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://free-legal-education-in-india.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  adapter: netlify(),
 });
